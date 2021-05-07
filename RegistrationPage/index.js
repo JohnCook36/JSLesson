cc74b9a84da8d8c9f18d4687872f45c8b1649dd3
JohnCook36 <bormancmexv@gmail.com>
2021-05-08 02:05:50 +0300

    add label to RegistrationForm and some points

---
+++ RegistrationPage/index.js
@@ -0,0 +1,24 @@
+function checkForm(el) {
+    let name = el.name.value
+    let surname = el.surname.value
+    let pass = el.pass.value
+    let phone = el.phone.value
+
+    // let fail = ''
+    // if(name === '') {
+    //     fail = 'заполните все поля'
+    // } else if( name.length <= 1 || name.length > 20)
+    //     fail = 'Введите коректное имя';
+    //  else if(pass.split('&').length > 1)
+    //     fail = 'Введён недопустимый символ & '
+    //
+    // if (fail != '') {
+    //     document.getElementById('fail').innerHTML = fail
+    //
+    //     return false
+    // } else {
+    //     alert('Все данные кректно заполнены')
+    //     return true
+    // }
+
+}
