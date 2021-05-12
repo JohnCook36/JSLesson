6adb3810c65a546b91857759a60f12f376d8c5eb
JohnCook laptop <bormancmexv@gmail.com>
2021-05-13 01:50:57 +0300

    add save in localStorage

--- RegistrationPage/index.js
+++ RegistrationPage/index.js
@@ -4,21 +4,12 @@ function checkForm(el) {
     let pass = el.pass.value
     let phone = el.phone.value
 
-    // let fail = ''
-    // if(name === '') {
-    //     fail = 'заполните все поля'
-    // } else if( name.length <= 1 || name.length > 20)
-    //     fail = 'Введите коректное имя';
-    //  else if(pass.split('&').length > 1)
-    //     fail = 'Введён недопустимый символ & '
-    //
-    // if (fail != '') {
-    //     document.getElementById('fail').innerHTML = fail
-    //
-    //     return false
-    // } else {
-    //     alert('Все данные кректно заполнены')
-    //     return true
-    // }
-
+    let info = {
+        'name': name,
+        'surname': surname,
+        'pass': pass,
+        'phone': phone
+    }
+    localStorage.setItem('info', JSON.stringify(info))
+    localStorage.getItem('info')
 }
